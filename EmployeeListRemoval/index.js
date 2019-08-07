2b2b8bb9d7b9cbcfc1467812d8e800c9626a5822
Lewis Williams <lewis.williams@transformuk.com>
2019-08-07 15:18:09 +0100

    validation fix

--- EmployeeListRemoval/index.js
+++ EmployeeListRemoval/index.js
@@ -1,15 +1,21 @@
-const {askQuestion} = require("./removeEmployee");
-const {DeleteEmployee} = require('./removeEmployee')
+const {DeleteEmployee, askQuestion} = require('./removeEmployee')
 
-DeleteEmployee('./Emp.txt')
-    .then(data => {
-        const filter = data.filter( i => i !== '');
-        return filter;
-    }).then( users => {
-        users.forEach(emp => console.log(emp))
-        return users;
-        }).then( userToDelete => askQuestion(userToDelete))
-    .catch(err => console.log(err));
-
+// DeleteEmployee('./Emp.txt')
+//     .then(data => {
+//         const filter = data.filter( i => i !== '');
+//         return filter;
+//     }).then( users => {
+//         users.forEach(emp => console.log(emp))
+//         return users;
+//         }).then( userToDelete => askQuestion(userToDelete))
+//     .catch(err => console.log(err));
 
 
+
+const DelEmployee = async (fileLocation) => {
+    const Delete = await DeleteEmployee(fileLocation);
+    console.log(Delete.join('\n'))
+    return  await askQuestion(Delete);
+};
+
+DelEmployee('./Emp.txt').finally(() => console.log('Done'));
